833d57ca6df5049fe3e4c667dd1d5a4fa96369da
Edwin Babu <edwinbbu@gmail.com>
2018-07-04 08:26:12 +0530

    validation in progress

--- static/client/signupValidator.js
+++ static/client/signupValidator.js
@@ -1,8 +1,11 @@
 onsubmit="return validateForm()"
 document.querySelector("#signupform").addEventListener('submit',(event)=>{
-    var name=event.target.name.value;
-    if(name=="abc"){
-        name='';
+    let name=event.target.name.value;
+    console.log(typeof(name)+":"+name);
+    if(name==''){
+        let html='<input type="text" class="form-control" name="name" placeholder="Name"\>\
+                  <p style="color:#b11616;font-size: 14px;">*Provide a name<p>';
+        $("#name").html(html);
         event.preventDefault();
     }
     console.log(event.target.email.value);
