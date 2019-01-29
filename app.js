9022e129911a58ac69f91081583ba70ebca4e4a7
edwinbbu <edwinbbu@gmail.com>
2019-01-29 08:07:04 +0530

    http to https

--- app.js
+++ app.js
@@ -1,59 +1,68 @@
 //Imports
-require('dotenv-extended').load();
-var express = require('express');
-var path = require('path');
-var parser = require('body-parser')
-var passport = require('passport');
-var session = require('express-session');
-var expressLayouts = require('express-ejs-layouts');
-var flash = require('connect-flash');
-var morgan = require('morgan');
+require("dotenv-extended").load();
+var express = require("express");
+var path = require("path");
+var parser = require("body-parser");
+var passport = require("passport");
+var session = require("express-session");
+var expressLayouts = require("express-ejs-layouts");
+var flash = require("connect-flash");
+var morgan = require("morgan");
 
 // basic setup
 var app = express();
-var port = process.env.port || 8000 ;
+var port = process.env.PORT || 8000;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'templates'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "templates"));
 //app.set('view options', { layout: 'layout.ejs' });
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 app.use(parser.json());
-app.use(parser.urlencoded({ extended: false }))
+app.use(parser.urlencoded({ extended: false }));
 app.use(expressLayouts);
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use(session({
-    secret:'Blog-App',
+app.use(
+  session({
+    secret: "Blog-App",
     saveUninitialized: true,
     resave: true
-}));
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash()); // flash messages
 
-app.use(function (req, res, next) {
-    res.locals.success = req.flash('success');
-    res.locals.info = req.flash('info');
-    res.locals.error = req.flash('error');
-    res.locals.user = req.user || null;
-    next();
+app.use(function(req, res, next) {
+  res.locals.success = req.flash("success");
+  res.locals.info = req.flash("info");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
+  next();
+});
+if (process.env.STAGE != dev) {
+  app.use(function(req, res, next) {
+    if (req.get("X-Forwarded-Proto") !== "https") {
+      res.redirect("https://" + req.get("Host") + req.url);
+    } else next();
   });
+}
 
 // require files
-var indexRouter = require('./routes/indexRouter');
-var loginRouter = require('./routes/loginRouter');
-var notesRouter = require('./routes/notesRouter');
-var signupRouter = require('./routes/signupRouter');
-var profileRouter = require('./routes/profileRouter');
+var indexRouter = require("./routes/indexRouter");
+var loginRouter = require("./routes/loginRouter");
+var notesRouter = require("./routes/notesRouter");
+var signupRouter = require("./routes/signupRouter");
+var profileRouter = require("./routes/profileRouter");
 
-app.use('/', indexRouter);
-app.use('/login', loginRouter);
-app.use('/notes', notesRouter);
-app.use('/signup', signupRouter);
-app.use('/profile', profileRouter);
+app.use("/", indexRouter);
+app.use("/login", loginRouter);
+app.use("/notes", notesRouter);
+app.use("/signup", signupRouter);
+app.use("/profile", profileRouter);
 
-app.listen(port, function (error) {
-    console.log('Server running on port ' + port);
+app.listen(port, function(error) {
+  console.log("Server running on port " + port);
 });
